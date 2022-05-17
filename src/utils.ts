ceb07901c25ae5377ebcbb1edd8a174f9538d9fb
Eric <37158449+eluce2@users.noreply.github.com>
2022-05-17 12:56:32 -0700

    add utility function - remove table names

---
+++ src/utils.ts
@@ -0,0 +1,26 @@
+import { S, L, U } from "ts-toolbelt";
+
+type TransformedFields<T extends Record<string, any>> = U.Merge<
+  {
+    [Field in keyof T]: {
+      [Key in Field extends string
+        ? L.Last<S.Split<Field, "::">>
+        : Field]: T[Field];
+    };
+  }[keyof T]
+>;
+
+export function removeFMTableName<T extends Record<string, any>>(
+  obj: T
+): TransformedFields<T> {
+  const newObj: any = {};
+  for (const key in obj) {
+    if (key.includes("::")) {
+      const newKey = key.split("::")[1];
+      newObj[newKey] = obj[key];
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
