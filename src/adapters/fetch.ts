8f56769efb23b219c1affc7c239ff45c2b60a9fe
HosakaKeigo <74914629+HosakaKeigo@users.noreply.github.com>
2024-12-27 11:36:38 +0900

    [fix] Remove redundant `setToken`
    Obviate `setToken` when saved token is avaliable

--- src/adapters/fetch.ts
+++ src/adapters/fetch.ts
@@ -65,7 +65,6 @@ export class FetchAdapter extends BaseFetchAdapter {
       this.tokenStore.setToken(this.getTokenKey(), token);
     }
 
-    this.tokenStore.setToken(this.getTokenKey(), token);
     return token;
   };
 
